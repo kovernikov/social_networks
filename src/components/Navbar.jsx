03b8a4573f918a9742611b28b3d755cb36489723
Alexej <kovernikov@post.cz>
2021-04-29 20:39:02 +0200

    12 profile, header, navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <a>Profile</a>
+            </div>
+            <div>
+                <a>Messages</a>
+            </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Musik</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
+        </nav>
+    );
+}
+export default Navbar;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img
+                    src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"/>
+            </div>
+            <div>
+                {/*<img src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Kot-019.jpg"/>*/}
+                ava+discripion
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Profile;
