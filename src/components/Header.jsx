c89651ae4c41de9cc4e1ebdaddb7fae708743f61
Alexej <kovernikov@post.cz>
2021-04-29 20:39:02 +0200

    12 profile, header, navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_-I86af1RynC0u9KgHKUD-H6tCwT_Cf_P2Q&usqp=CAU"/>
+        </header>
+    );
+
+}
+
+export default Header;
