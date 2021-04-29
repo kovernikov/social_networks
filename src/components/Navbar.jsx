30b9fa73cdbdfb09b14bb11e3bc117fc0cb6fc99
Alexej <kovernikov@post.cz>
2021-04-29 22:50:15 +0200

    14 module css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
+import s from './Navbar.module.css';
 
 const Navbar = () => {
     return (
         <nav>
-            <div>
+            <div className={s.item}>
                 <a>Profile</a>
             </div>
-            <div>
+            <div className={`${s.item} ${s.active}`}>
                 <a>Messages</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a>News</a>
             </div>
-            <div>
-                <a>Musik</a>
+            <div className={s.item}>
+                <a>Music</a>
             </div>
-            <div>
+            <div className={s.item}>
                 <a>Settings</a>
             </div>
         </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img
                     src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350"/>
@@ -16,11 +17,11 @@ const Profile = () => {
                 <div>
                     New post
                 </div>
-                <div>
-                    <div>
+                <div className={s.posts}>
+                    <div className={`${s.item} ${s.active}`}>
                         post1
                     </div>
-                    <div>
+                    <div className={s.item}>
                         post2
                     </div>
                 </div>
