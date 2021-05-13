c1cafbd885d25b84af7e05ff73458fc54e7139dd
Alexej <kovernikov@post.cz>
2021-05-14 00:26:36 +0200

    lesson_25

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,36 +2,35 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 import s from './Dialog.module.css'
 
-type DialogItemProps = {
-    name: string
-    id: number
-}
 
-const DialogItem = (props: DialogItemProps) => {
+const DialogItem: React.FC<DialogItemProps> = (props) => {
     let path = '/dialogs/' + props.id;
 
     return (
         <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path} activeClassName={s.active}>{props.name}</NavLink>
+            <NavLink to={path} className={s.dialog} activeClassName={s.active}>{props.name}</NavLink>
         </div>
     )
 }
-
+type DialogItemProps = {
+    name: string
+    id: number
+}
 type MessageItemProps = {
     message: string
 }
-
-const Message = (props: MessageItemProps) => {
-    return <div className={s.dialog}> {props.message} </div>
-}
-
 type DialogsProps = {
     DialogsItem: Array<DialogItemProps>
     MessagesItem: Array<MessageItemProps>
 }
 
+
+const Message: React.FC<MessageItemProps> = (props) => {
+    return <div className={s.dialog}> {props.message} </div>
+}
+
 const Dialogs = (props: DialogsProps) => {
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Dimych'},
         {id: 2, name: 'Andrey'},
         {id: 3, name: 'Sveta'},
@@ -40,7 +39,7 @@ const Dialogs = (props: DialogsProps) => {
         {id: 6, name: 'Viktor'},
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hello!!!'},
         {id: 2, message: 'Haw are you?'},
         {id: 3, message: 'This is my friend.'},
@@ -48,23 +47,17 @@ const Dialogs = (props: DialogsProps) => {
         {id: 5, message: 'yo'},
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                {dialogsElements}
 
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message}/>
-                <Message message={messagesData[1].message}/>
-                <Message message={messagesData[2].message}/>
-                <Message message={messagesData[3].message}/>
-                <Message message={messagesData[4].message}/>
+                {messagesElements}
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import s from './MyPosts.module.css';
-import Post from './Post/Post'
+import Post, {PostPropsType} from './Post/Post'
+
 
 
 const MyPosts = () => {
 
-    let postData = [
-        {id: 1, message: 'Hi, how are you', likesCount: '20'},
-        {id: 2, message: 'It\'s my first post', likesCount: '15'},
+    let posts = [
+        {
+            id: 1,
+            message: 'Hi, how are you',
+            likesCount: '12',
+            img: 'https://i.guim.co.uk/img/media/bf7e98bd289d189e5b86d3f406f740368c663a6a/85_0_2544_1527/master/2544.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=98cd2e37e0df5d552473c329d88349ee'
+        },
+        {
+            id: 2,
+            message: 'It\'s my first post',
+            likesCount: '15',
+            img: 'https://i.guim.co.uk/img/media/bf7e98bd289d189e5b86d3f406f740368c663a6a/85_0_2544_1527/master/2544.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=98cd2e37e0df5d552473c329d88349ee'
+        }
     ]
-    let imgData = [
-        {id: 1, foto: 'https://i.guim.co.uk/img/media/bf7e98bd289d189e5b86d3f406f740368c663a6a/85_0_2544_1527/master/2544.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=98cd2e37e0df5d552473c329d88349ee'},
 
-    ]
+
+    let postElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} img={p.img}/>);
 
     return (
         <div className={s.postsBlock}>
@@ -26,10 +36,7 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}
-                      img={imgData[0].foto}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}
-                      img={imgData[0].foto}/>
+                {postElements}
             </div>
         </div>
     )
