a60b58c2cf0191cb54158eae4ebdc040f22ad199
Alexej <kovernikov@post.cz>
2021-05-18 22:03:53 +0200

    lesson_31_createRef

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -16,6 +16,13 @@ const Dialogs: React.FC<DialogsProps> = (props) => {
     let dialogsElements = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
     let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
 
+    let newMessageElement = React.createRef<HTMLTextAreaElement>()
+
+    let addMessage = () => {
+        let text = newMessageElement.current?.value;
+        alert(text)
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -25,6 +32,9 @@ const Dialogs: React.FC<DialogsProps> = (props) => {
             <div className={s.messages}>
                 {messagesElements}
             </div>
+            <div><textarea ref={newMessageElement}></textarea> </div>
+            <div>
+                <button onClick={addMessage}>Add text</button> </div>
         </div>
     )
 }
