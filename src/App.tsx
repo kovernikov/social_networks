035eda0023ea7484e7fe303e83c00ae77503b5d9
Alexej <kovernikov@post.cz>
2021-07-13 23:36:08 +0200

    lesson_60

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC<AppPropsType> = (props) => {
             <div className={'app-wrapper-content'}>
                 <Route path={'/dialogs'}
                        render={() => <DialogsContainer />}/>
-                <Route path={'/profile'}
+                <Route path={'/profile/:userId'}
                        render={() => <ProfileContainer />}/>
                 <Route path={'/users'}
                        render={() => <UsersContainer/>}/>
