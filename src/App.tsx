2d314c5339ec5a2a702fc3921dd294cbec2329b6
Alexej <kovernikov@post.cz>
2021-10-30 12:17:59 +0200

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,76 @@
-import React from 'react';
-import {Route} from 'react-router-dom';
+import React, {ComponentType} from 'react';
+import {Route, withRouter} from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
-import Profile from './components/Profile/Profile';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Setting from './components/Setting/Setting';
-// import store from './redux/store';
 import Friends from './components/Navbar/Friends/Friends';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
-import { Users } from './components/Users/Users';
-import { UsersContainer } from './components/Users/UsersContainer';
-// import {store} from './redux/redux-store';
+import UsersContainer from './components/Users/UsersContainer';
+import {HeaderContainer} from './components/Header/HeaderContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import {LoginPage} from './components/Login/Login';
+import {connect} from 'react-redux';
+import {initializeApp} from './redux/appReducer';
+import {compose} from 'redux';
+import {AppStateType} from './redux/redux-store';
+import {Preloader} from './common/Preloader/Preloader';
 
-export type AppPropsType = {
+export const path = {
+	PROFILE: '/profile',
+	DIALOGS: '/dialogs',
+	CHAT: '/chat',
+	DEVELOPERS: '/developers',
+	LOGIN: '/login',
+}
+export type AppPropsType = MapDispatchType & MapStateType
 
+class App extends React.Component<AppPropsType> {
+
+	componentDidMount() {
+		this.props.initializeApp()
+	}
+	
+	
+	render() {
+		// todo: const state = props.store.getState()
+		if(this.props.initialized) {
+			return <Preloader/>
+		}
+
+		return (
+			<div className="app-wrapper">
+				<HeaderContainer/>
+				<Navbar/>
+				<div className={'app-wrapper-content'}>
+					<Route path={'/dialogs'}
+						   render={() => <DialogsContainer/>}/>
+					<Route path={'/profile/:userId?'}
+						   render={() => <ProfileContainer/>}/>
+					<Route path={'/users'}
+						   render={() => <UsersContainer/>}/>
+					<Route path={'/login'}
+						   render={() => <LoginPage/>}/>
+					<Route path={'/news'} render={() => <News/>}/>
+					<Route path={'/music'} render={() => <Music/>}/>
+					<Route path={'/setting'} render={() => <Setting/>}/>
+					<Route path={'/friends'} render={() => <Friends/>}/>
+				</div>
+			</div>
+		)
+	}
+}
+type MapDispatchType = {
+	initializeApp: () => void
+	// logout: () => void
+}
+type MapStateType = {
+	initialized: boolean
 }
 
-const App: React.FC<AppPropsType> = (props) => {
-    // const state = props.store.getState()
+const mapStateToProps = (state: AppStateType): MapStateType => ({
+	initialized: state.app.initialized
+})
 
-    // @ts-ignore
-    return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className={'app-wrapper-content'}>
-                <Route path={'/dialogs'}
-                       render={() => <DialogsContainer />}/>
-                <Route path={'/profile'}
-                       render={() => <Profile />}/>
-                <Route path={'/users'}
-                       render={() => <UsersContainer/>}/>
-                <Route path={'/news'} render={() => <News/>}/>
-                <Route path={'/music'} render={() => <Music/>}/>
-                <Route path={'/setting'} render={() => <Setting/>}/>
-                <Route path={'/friends'} render={() => <Friends />}/>
-            </div>
-        </div>
-    )
-}
-
-
-export default App;
+export default compose<ComponentType>(withRouter, connect<MapStateType, MapDispatchType,{},AppStateType>(mapStateToProps, {initializeApp}))(App);
